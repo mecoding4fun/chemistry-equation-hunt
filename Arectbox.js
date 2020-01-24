b327020065c9a6cdbf775a158484804280132c18
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-01-24 18:35:49 +0530

    Add files via upload

---
+++ Arectbox.js
@@ -0,0 +1,28 @@
+class Arectbox{
+    constructor(pointA,bodyB){
+
+        var constraintoptions = {
+            pointA:pointA,
+       bodyB:bodyB,
+    
+
+        }
+      //  this.body = Bodies.rectangle(75,height-125,60,30, options);
+        this.cannon = Constraint.create(constraintoptions);
+     /*   this.width = width;
+        this.height = height;
+        World.add(world, this.body);*/
+        World.add(world,this.cannon);
+    };
+
+    attach(body){
+        this.cannon.pointA = body;
+    }
+
+    shoot(){
+        this.cannon.pointA = null;
+    }
+
+
+
+}   
---
+++ sketch.js
@@ -0,0 +1,150 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ground, cannon,ball,cannon2;
+var moving_bot,bot_img;
+var angle = 0;
+var A_letter_img,A_letter;
+var B_letter_img,B_letter;
+var C_letter_img,C_letter;
+var H_letter_img,H_letter;
+var H_letter_img_second,H_letter_second;
+var O_letter_img,O_letter;
+var O_letter_img_second,O_letter_second;
+var water_img,water_question;
+var score = 40;
+
+
+function preload() {
+  A_letter_img = loadImage("alphabet1.png");
+  B_letter_img = loadImage("alphabet2.png");
+  C_letter_img = loadImage("alphabet3.png");
+  H_letter_img = loadImage("alphabet8.png");
+  H_letter_img_second = loadImage("alphabet8.png");
+  O_letter_img = loadImage("alphabet15.png");
+  O_letter_img_second = loadImage("alphabet15.png");
+  water_img = loadImage("water.png");
+ bot_img = loadImage("bird.png");
+}
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+
+  moving_bot = createSprite(85,height-115,40,40);
+  moving_bot.addImage("botball",bot_img)
+  moving_bot.velocity.x = 0;
+  moving_bot.debug = true;
+
+
+  A_letter = createSprite(width-200,height-120);
+  A_letter.addImage("letterA",A_letter_img);
+  A_letter.scale = 0.5;
+  
+  B_letter = createSprite(width-40,height-240);
+  B_letter.addImage("letterB",B_letter_img);
+  B_letter.scale = 0.5;
+
+  C_letter = createSprite(width-40,height-540);
+  C_letter.addImage("letterB",C_letter_img);
+  C_letter.scale = 0.5;
+
+  H_letter = createSprite(width-716,height-278);
+  H_letter.addImage("letterH",H_letter_img);
+  H_letter.scale = 0.5;
+
+  H_letter_second = createSprite(width-716,height-378);
+  H_letter_second.addImage("letterh",H_letter_img_second);
+  H_letter_second.scale = 0.5;
+  H_letter_second.debug = true;
+
+
+  O_letter = createSprite(width-410,height-500);
+  O_letter.addImage("letterO",O_letter_img);
+  O_letter.scale = 0.5;
+
+  O_letter_second = createSprite(width-410,height-550);
+  O_letter_second.addImage("lettero",O_letter_img_second);
+  O_letter_second.scale = 0.5;
+
+  water_question= createSprite(100,70);
+  water_question.addImage("water", water_img);
+  water_question.scale = 0.5;
+  
+      ground = new Ground(width/2, height-30,width, 50); 
+      cannon2 = new Cannon2(75,height-125,60,30);
+      ball = new Ball(width/2,height-125,50,50);
+      cannon = new Arectbox({x : 200,y:100},ball.body);
+      // ball.velocity.x = 3;
+      // cannon2.debug = true;
+
+
+
+  
+}
+
+function draw() {
+  background(255);
+  Engine.update(engine);
+  ground.display();
+  //cannon.display();
+  cannon2.display();
+  textSize(100);
+  text("Score : " + score,width-50,50);
+  console.log(score);
+
+  if(mousePressedOver){
+   moving_bot.velocity.x = mouseX/160;
+   moving_bot.velocity.y = -mouseY/160;
+  }
+  if(moving_bot.isTouching(H_letter_second)){
+    score = score-10;
+  }
+  if (moving_bot.isTouching(C_letter)){
+    C_letter.destroy();
+    score = score +10;
+  }
+  if ( moving_bot.x > width || moving_bot.y > height  && mousePressedOver ){
+    moving_bot.x = 75;
+    moving_bot.y = height-123;
+  }
+  if (moving_bot.isTouching(A_letter)){
+  score = score -10;
+  }
+  if (moving_bot.isTouching(B_letter)){
+    score = score -10;
+  }
+  if (moving_bot.isTouching(H_letter)){
+    score = score -10;
+  }
+  if (moving_bot.isTouching(O_letter)){
+    O_letter.destroy();
+    score = score +10;
+  }
+  if (moving_bot.isTouching(O_letter_second)){
+    O_letter_second.destroy();
+    score = score +10;
+  }
+
+  ball.display();
+  water_question.display();
+  moving_bot.display();
+  H_letter.display();
+  H_letter_second.display();
+  A_letter.display();
+  B_letter.display();
+  C_letter.display();
+  O_letter.display();
+  O_letter_second.display();
+
+  cannon.attach(ball.body);
+
+
+  
+}
